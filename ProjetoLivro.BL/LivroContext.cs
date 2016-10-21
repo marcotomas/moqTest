c7ddd0e62f8beff700c63e4efd16afe8c3d5cc0d
Marco Tomás <marco.tomas@altitude.com>
2016-10-21 11:01:32 +0100

    First Commit

---
+++ ProjetoLivro.BL/LivroContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace ProjetoLivro.BL
+{
+    public class LivroContext : DbContext
+    {
+        public virtual DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ ProjetoLivro.BL/LivroService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoLivro.BL
+{
+    public class LivroService
+    {
+        private LivroContext _context { get; set; }
+        public LivroService(LivroContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> GetLivrosAtivos()
+        {
+            return _context.Livros.Where(a => a.Status == true).ToList();
+        }
+
+        public Task<List<Livro>> GetLivrosAtivosAsync()
+        {
+            return _context.Livros.Where(a => a.Status == true).ToListAsync();
+        }
+
+        public void Salvar(Livro livro)
+        {
+            _context.Livros.Add(livro);
+            _context.SaveChanges();
+        }
+    }
+}
